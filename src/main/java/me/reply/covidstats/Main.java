044896fc0eabdd69eb8d74ae9462126192a0225f
lorenzo-patane <lorenzo.patane01@gmail.com>
2020-04-22 21:35:17 +0200

    Download database from http link (for heroku)

--- src/main/java/me/reply/covidstats/Main.java
+++ src/main/java/me/reply/covidstats/Main.java
@@ -1,19 +1,36 @@
 package me.reply.covidstats;
 
+import org.apache.commons.io.FileUtils;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            downloadDatabase();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
-
         try {
             telegramBotsApi.registerBot(new Bot());
         } catch (TelegramApiRequestException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void downloadDatabase() throws IOException {
+        String databaseLink = System.getenv("DATABASE_LINK");
+        if(databaseLink == null)
+            return;
+        FileUtils.copyURLToFile(new URL(databaseLink),new File("database.db"));
     }
 }
